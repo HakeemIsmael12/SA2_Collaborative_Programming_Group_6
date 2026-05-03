8b880a14102786722ac42ea12a12197bb2346b45
HakeemIsmael12 <hsiismael@cmc.pshs.edu.ph>
2026-05-03 21:43:43 +0800

    Hopefully the final update on code

--- Untitled-1.py
+++ Untitled-1.py
@@ -20,3 +20,27 @@ Q1 = tQ1
 Q2 = ((Q1 + 2) * tQ2) / 3
 Q3 = ((Q2 + 2) * tQ3) / 3
 Q4_Final_Grade = ((Q3 + 2) * tQ4) / 3
+
+# to find the Adjectival Equivalent of your Final Grade
+if (Q4_Final_Grade >= 96 and Q4_Final_Grade <= 100):
+    print("EQUIVALENT: 1.00, ADJECTIVE: EXCELLENT")
+elif (Q4_Final_Grade >= 90 and Q4_Final_Grade <= 95.99):
+    print("EQUIVALENT: 1.25, ADJECTIVE: VERY GOOD")
+elif (Q4_Final_Grade >= 84 and Q4_Final_Grade <= 89.99):
+    print("EQUIVALENT: 1.50, ADJECTIVE: VERY GOOD")
+elif (Q4_Final_Grade >= 78 and Q4_Final_Grade <= 83.99):
+    print("EQUIVALENT: 1.75, ADJECTIVE: GOOD")
+elif (Q4_Final_Grade >= 72 and Q4_Final_Grade <= 77.99):
+    print("EQUIVALENT: 2.00, ADJECTIVE: GOOD")
+elif (Q4_Final_Grade >= 66 and Q4_Final_Grade <= 71.99):
+    print("EQUIVALENT: 2.25, ADJECTIVE: SATISFACTORY")
+elif (Q4_Final_Grade >= 60 and Q4_Final_Grade <= 65.99):
+    print("EQUIVALENT: 2.50, ADJECTIVE: SATISFACTORY")
+elif (Q4_Final_Grade >= 55 and Q4_Final_Grade <= 59.99):
+    print("EQUIVALENT: 2.75, ADJECTIVE: FAIR")
+elif (Q4_Final_Grade >= 50 and Q4_Final_Grade <= 54.99):
+    print("EQUIVALENT: 3.00, ADJECTIVE: FAIR")
+elif (Q4_Final_Grade >= 40 and Q4_Final_Grade <= 49.99):
+    print("EQUIVALENT: 4.00, ADJECTIVE: FAILED ON CONDITION")
+else:
+    print("EQUIVALENT: 5.00, ADJECTIVE: FAILED")
